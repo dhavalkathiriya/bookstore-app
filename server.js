b5158b4014ed5346ca8ff49bdb90c7323371be34
dhaval kathiriya <112376785+dhavalkathiriya@users.noreply.github.com>
2023-06-02 15:51:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ import cors from 'cors'
 import connectDB from './db/db'
 import postRoute from './routes/postRoute'
 import { PORT } from './config'
+import path from "path";
+
 
 
 connectDB()
 const app =express()
 app.use(cors())
 app.use(express.json())
-
 app.use("/api/post",postRoute)
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 app.listen(PORT,() =>{
 console.log(`server is running ${PORT}`);
