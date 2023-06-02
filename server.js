797917be0281ef10673c8e138d216abf10002f48
dhaval kathiriya <112376785+dhavalkathiriya@users.noreply.github.com>
2023-06-02 16:09:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/post",postRoute)
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.static(path.join(__dirname, "./client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
 });
 
 app.listen(PORT,() =>{
